8076fe1a974534d6d91712426ae460a31b273313
Momchil Marinov <momchil.marinov@redis.com>
2026-04-14 09:43:19 +0300

    Add --yes flag to 04 script

--- utils/releasetools/04_release_hash.sh
+++ utils/releasetools/04_release_hash.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
-if [ $# != "1" ]
+
+# Parse optional flags
+AUTO_CONFIRM=0
+VERSION_TAG=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -y|--yes)
+            AUTO_CONFIRM=1
+            shift
+            ;;
+        *)
+            VERSION_TAG="$1"
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$VERSION_TAG" ]
 then
-    echo "Usage: ./utils/releasetools/04_release_hash.sh <version_tag>"
+    echo "Usage: ./utils/releasetools/04_release_hash.sh [-y|--yes] <version_tag>"
     exit 1
 fi
 
-SHA=$(curl -s http://download.redis.io/releases/redis-${1}.tar.gz | shasum -a 256 | cut -f 1 -d' ')
-ENTRY="hash redis-${1}.tar.gz sha256 $SHA http://download.redis.io/releases/redis-${1}.tar.gz"
+SHA=$(curl -s http://download.redis.io/releases/redis-${VERSION_TAG}.tar.gz | shasum -a 256 | cut -f 1 -d' ')
+ENTRY="hash redis-${VERSION_TAG}.tar.gz sha256 $SHA http://download.redis.io/releases/redis-${VERSION_TAG}.tar.gz"
 echo $ENTRY >> ../redis-hashes/README
-echo "Press any key to commit, Ctrl-C to abort)."
-read yes
-(cd ../redis-hashes; git commit -a -m "${1} hash."; git push)
+
+if [ $AUTO_CONFIRM -eq 0 ]; then
+    echo "Press any key to commit, Ctrl-C to abort)."
+    read yes
+fi
+
+(cd ../redis-hashes; git commit -a -m "${VERSION_TAG} hash."; git push)
